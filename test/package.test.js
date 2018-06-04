69a4f3911288eb2ff54b2760e95fd8a2dc74a9b0
Jared Hanson <jaredhanson@gmail.com>
2018-06-04 07:53:58 -0700

    Stub out tests.

--- test/package.test.js
+++ test/package.test.js
@@ -1,13 +1,25 @@
-/* global describe, it */
+/* global describe, it, expect */
 
-var pkg = require('..');
 var expect = require('chai').expect;
 
 
-describe('authnomicon-uma', function() {
+describe('@authnomicon/uma', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  describe('package.json', function() {
+    var json = require('../package.json');
+    
+    it('should have assembly metadata', function() {
+      expect(json.assembly.namespace).to.equal('org.authnomicon/uma');
+      
+      expect(json.assembly.components).to.have.length(1);
+      expect(json.assembly.components).to.include('http/rsreg/service');
+    });
+  });
+  
+  it('should throw if required', function() {
+    expect(function() {
+      var pkg = require('..');
+    }).to.throw(Error).with.property('code', 'MODULE_NOT_FOUND');
   });
   
 });
