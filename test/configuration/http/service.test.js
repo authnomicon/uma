cd404d7dfe9f901929c7bde5b22b389f75c6a65c
Jared Hanson <jaredhanson@gmail.com>
2021-10-16 20:52:16 -0700

    Add tests.

---
+++ test/configuration/http/service.test.js
@@ -0,0 +1,27 @@
+/* global describe, it, expect */
+
+var expect = require('chai').expect;
+var factory = require('../../../com/configuration/http/service');
+
+
+describe('configuration/http/service', function() {
+  
+  it('should export factory function', function() {
+    expect(factory).to.be.a('function');
+  });
+  
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.equal('http://i.bixbyjs.org/http/Service');
+    expect(factory['@path']).to.equal('/.well-known/uma-configuration');
+  });
+  
+  it('should create service', function() {
+    function configurationHandler() {};
+  
+    var service = factory(configurationHandler);
+    
+    expect(service).to.be.a('function');
+    expect(service.length).to.equal(3);
+  });
+  
+});
